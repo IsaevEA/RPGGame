c78a98971593662da290c861c261bd17c6183cd7
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-09-16 17:36:22 +0600

    hero and item layout

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,24 @@
-public class Inventory {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Inventory {
+    private Map<String, Item> itemMap = new HashMap<>();
+    private int capacity = 50;
+
+    public void addItem(Item item){
+        if (capacity > 0){
+            itemMap.put(item.getName(), item);
+            capacity -= item.getWeight();
+        }else System.out.println("Недостаточно место");
+    }
+    public void removeItem(String nameItem){
+        if (itemMap.containsKey(nameItem)){
+            capacity += itemMap.get(nameItem).getWeight();
+            itemMap.remove(nameItem);
+        }
+    }
+
+    public void printAllItem(){
+        System.out.println(itemMap);
+    }
 }
